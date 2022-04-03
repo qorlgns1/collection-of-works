a81503cfd23f550e6eabba40ba43f43f65675c8f
qorlgns1 <rlgns0610@gmail.com>
2022-04-03 10:06:53 +0900

    refactor: 비밀번호만입력시 로그인 활성화되는 이슈 처리
    1. 비밀번호만입력시 로그인 활성화되는 이슈 처리
    2. 코드 정리

--- instagram/js/login.js
+++ instagram/js/login.js
@@ -13,30 +13,31 @@ const checkLogin = (info) => {
   }
 };
 
-userId.addEventListener("keyup", (e) => {
-  if (e.target.value && userPw.value) {
-    loginBtn.classList.add("btn-active");
-  } else {
-    loginBtn.classList.remove("btn-active");
-  }
-});
+const activeBtn = (userInfo) => {
+  userInfo[0].addEventListener("keyup", (e) => {
+    if (e.target.value && userInfo[1].value) {
+      loginBtn.classList.add("btn-active");
+    } else {
+      loginBtn.classList.remove("btn-active");
+    }
+  });
+};
 
-userPw.addEventListener("keyup", (e) => {
-  if (e.target.value) {
-    loginBtn.classList.add("btn-active");
-  } else {
-    loginBtn.classList.remove("btn-active");
-  }
-});
+const resetUserInfo = (id, pw) => {
+  id.value = "";
+  pw.value = "";
+  loginBtn.classList.remove("btn-active");
+};
+
+activeBtn([userId, userPw]);
+activeBtn([userPw, userId]);
 
 loginBtn.addEventListener("click", (e) => {
   e.preventDefault();
 
   if (checkLogin(userId) && checkLogin(userPw)) {
-    userId.value = "";
-    userPw.value = "";
-    loginBtn.classList.remove("btn-active");
     alert("로그인 성공");
+    resetUserInfo(userId, userPw);
   } else {
     alert("로그인 실패");
   }
