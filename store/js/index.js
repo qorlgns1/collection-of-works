0994fa05975c4453ab550abaf02efc01b149024a
qorlgns1 <rlgns0610@gmail.com>
2022-04-10 18:15:04 +0900

    feat: 콜라 획득 기능 추가
    mini display 창에 콜라가 있는 경우 획득을 누르면 획득한 음료에 들어가도록 기능 추가

--- store/js/index.js
+++ store/js/index.js
@@ -3,9 +3,11 @@ import { insertMoney } from "./money/insertMoney.js";
 import { depositMoney } from "./money/depositMoney.js";
 import { insertCola } from "./cola/insertCola.js";
 import { rollbackCola } from "./cola/rollbackCola.js";
+import { buyCola } from "./cola/buyCola.js";
 
 firstVisitChargeMoney();
 insertMoney();
 depositMoney();
 insertCola();
 rollbackCola();
+buyCola();
